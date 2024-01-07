79072b7d47581b3d9cdfb48d2d99d3268842928b
SmirnovT <timsmir80@gmail.com>
2024-01-07 16:01:48 +0300

    ...

---
+++ homework/timur_smirnov/Homework_3/z_1.py
@@ -0,0 +1,5 @@
+a = 4
+b = 5
+print(f'Сумма a + b = {a + b}')
+print(f'Разность a - b = {a - b}')
+print(f'Произведение a * b = {a * b}')
---
+++ homework/timur_smirnov/Homework_3/z_3.py
@@ -0,0 +1,6 @@
+import math
+
+a = 4
+b = 5
+print(f'Среднее арифметическое a и b = {(a + b) / 2}')
+print(f'Среднее геометрическое a и b = {math.sqrt(a * b)}')
