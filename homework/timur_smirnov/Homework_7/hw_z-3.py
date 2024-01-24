8d6ee18286bd3e07550ff571aefc28c66cde8b1e
Timuchi <127180955+Timuchi@users.noreply.github.com>
2024-01-24 13:07:54 +0300

    add Homework_7 new

---
+++ homework/timur_smirnov/Homework_7/hw_z-3.py
@@ -0,0 +1,11 @@
+def extract_number_and_add(text, add_value):
+    number = int(text.split(':')[-1].strip())
+    return number + add_value
+results = [
+    "результат операции: 42",
+    "результат операции: 514",
+    "результат работы программы: 9"
+]
+for result in results:
+    new_number = extract_number_and_add(result, 10)
+    print(new_number)
