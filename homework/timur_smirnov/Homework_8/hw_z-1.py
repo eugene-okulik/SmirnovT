316e34a0549227667520127d8dab3232729d37b0
Timuchi <127180955+Timuchi@users.noreply.github.com>
2024-01-26 15:05:20 +0300

    add 1,2 homework

---
+++ homework/timur_smirnov/Homework_8/hw_z-1.py
@@ -0,0 +1,14 @@
+import random
+
+salary = int(input("Введите зарплату: "))
+flag = False
+#salary = 0
+bonus_input = input("Если бонус доступен, введите yes': ").lower()
+if bonus_input in ["yes", "да", '1']:
+    flag = True
+if flag:
+    bonus_amount = random.randint(5, 20) / 100 * salary
+    salary_bonus = salary + bonus_amount
+else:
+    salary_bonus = salary
+print(f"{salary}, {flag} - '${int(salary_bonus)}'")
