82c2534baf40cbaec6a809831d70eee01e7b489b
Timuchi <127180955+Timuchi@users.noreply.github.com>
2024-01-30 16:38:32 +0300

    Update hw9_dz-2.py
    update hw9_dz-2

--- homework/timur_smirnov/Homework_9/hw9_dz-2.py
+++ homework/timur_smirnov/Homework_9/hw9_dz-2.py
@@ -1,7 +1,7 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
                 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 # Создаем список с температурами выше 28 градусов
-hot_days = [i for i in temperatures if i > 28]
+hot_days = list(filter(lambda x: x > 28, temperatures))
 # Находим самую высокую и самую низкую температуру и вычисляем среднюю
 max_temp = max(hot_days)
 min_temp = min(hot_days)
